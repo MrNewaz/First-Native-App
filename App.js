4c5f1a873c5ac5f7cbcf801a5e686422b6fcbdfb
MrNewaz <saifur.saif@northsouth.edu>
2021-06-03 21:40:14 +0600

    68 done

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Text, TextInput } from 'react-native';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
+import AppTextInput from './app/components/AppTextInput';
+
 import Screen from './app/components/Screen';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
+import colors from './app/config/colors';
 
 export default function App() {
-  const [firstName, setFirstName] = useState('');
   return (
     <Screen>
-      <Text>{firstName}</Text>
-      <TextInput
-        secureTextEntry
-        onChangeText={(text) => setFirstName(text)}
-        placeholder='First Name'
-        style={{
-          borderBottomColor: '#ccc',
-          borderBottomWidth: 1,
-        }}
-      />
+      <AppTextInput icon='email' placeholder='User Email' />
     </Screen>
   );
 }
