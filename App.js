fb87835fd696032761f417b0381d17ee9282bb56
MrNewaz <saifur.saif@northsouth.edu>
2021-06-06 00:13:29 +0600

    part 2 video 7

--- App.js
+++ App.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import AccountScreen from './app/screens/AccountScreen';
+import React, { useEffect, useState } from 'react';
+import Screen from './app/components/Screen';
+import * as ImagePicker from 'expo-image-picker';
+import { Button, Image } from 'react-native';
 
 const App = () => {
-  return <AccountScreen />;
+  const [imageUri, setImageUri] = useState();
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) {
+      alert('You need to enable permission to access library');
+    }
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
+    } catch (error) {
+      console.log('Error reading an Image', error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title='Select Image' onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 };
 
 export default App;
