7fbb4c832d0b426836d9f9b2c7558a559157b75a
MrNewaz <saifur.saif@northsouth.edu>
2021-06-07 01:17:28 +0600

    22 done

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Button, Text } from 'react-native';
 import Screen from './app/components/Screen';
-import * as ImagePicker from 'expo-image-picker';
-import { Button, Image } from 'react-native';
-import ImageInput from './app/components/ImageInput';
-import ImageInputList from './app/components/ImageInputList';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button title='Click' onPress={() => navigation.navigate('TweetDetails')} />
+  );
+};
+
+const Tweets = ({ navigation }) => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+      <Link />
+    </Screen>
+  );
+};
+
+const TweetDetails = () => {
+  return (
+    <Screen>
+      <Text>Tweet Details</Text>
+    </Screen>
+  );
+};
+
+const Stack = createStackNavigator();
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Tweets' component={Tweets} />
+      <Stack.Screen name='TweetDetails' component={TweetDetails} />
+    </Stack.Navigator>
+  );
+};
 const App = () => {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default App;
