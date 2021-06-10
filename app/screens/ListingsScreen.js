8508af758a48aa9ac24ed9bfaca04e8f3abc84a7
MrNewaz <saifur.saif@northsouth.edu>
2021-06-10 12:41:14 +0600

    44 done

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -6,9 +6,12 @@ import routes from '../navigation/routes';
 import Screen from '../components/Screen';
 import Card from '../components/Card';
 import colors from '../config/colors';
+import AppText from '../components/AppText';
+import AppButton from '../components/AppButton';
 
 const ListingsScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadListings();
@@ -16,11 +19,20 @@ const ListingsScreen = ({ navigation }) => {
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+    if (!response.ok) return setError(true);
+
+    setError(false);
     setListings(response.data);
   };
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the listings</AppText>
+          <AppButton title='Retry' onPress={loadListings}></AppButton>
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing?.id?.toString()}
